17eb252514e6cd5a0a1a4a7193ec5a4df43bf587
Srgaming <67520105+Miartunduaga@users.noreply.github.com>
2023-10-16 16:52:43 -0500

    Tique no borre eso

---
+++ Index.py
@@ -0,0 +1,43 @@
+#PAG INDEX
+import tkinter as tk
+import practica as pa
+
+ventana = tk.Tk()
+ventana.geometry("600x600")
+ventana.title("INICIO DE CLASIFICACION")
+
+
+def abrir_Practica():
+    #Ocultar Pagina
+    ventana.withdraw()
+    #Mostrar Pagina
+    if variableDesicion.get()==0:
+        pa.ventanaP.title("PRACTICA")
+    elif variableDesicion.get()==1:
+        pa.ventanaP.title("CLASIFICACION")
+    
+    
+    pa.ventanaP.deiconify()
+    pa.sacarClase(variableDesicion.get())
+    print(variableDesicion.get()," ENTRENAMIENTO   0    ,    CLASIFICACION    1")
+    
+etiqueta = tk.Label(ventana,text="HOLA MUNDO",bg="green")
+etiqueta.pack(fill=tk.X)
+#command=lambda:saludo   permite sincronizar funciones.
+boton1=tk.Button(ventana, text="Practica",command=lambda:variableDesicion.set(0), width=15 ,height=5)
+boton1.place(x=270,y=150)
+
+boton2=tk.Button(ventana, text="CLASIFICACION",command=lambda :variableDesicion.set(1), width=15 ,height=5)
+boton2.place(x=270,y=250)
+
+
+boton3 =tk.Button(ventana,text="GUARDAR",command=abrir_Practica, width=10 ,height=5)
+boton3.place(x=500,y=500)
+
+def mostrarVentana():
+    ventana.deiconify()
+    pa.ocultarVentana()
+
+global variableDesicion
+variableDesicion =tk.IntVar()
+ventana.mainloop()
---
+++ perceptronProfe.py
@@ -0,0 +1,77 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+class PerceptronProfe:
+    grad_pesos=[]
+    def __init__(self, n):
+        self.n = n
+        self.bias =np.random.rand()
+        self.peso1 =np.random.rand()
+        self.peso2 =np.random.rand()
+        self.peso3 =np.random.rand()
+        self.tasaAprendizaje=0.5
+
+        self.historialPeso1=[self.peso1]
+        self.historialPeso2=[self.peso2]
+        self.historialPeso3=[self.peso3]
+        
+        #ESCALON
+    def propagacion(self,entradaPeso1,entradaPeso2,entradaPeso3):
+        
+        respuesta = self.peso1 * entradaPeso1 + self.peso2 * entradaPeso2 + self.peso3 * entradaPeso3 + self.bias
+
+        if respuesta>=0:
+            respuesta=1
+            
+        elif(respuesta<0):
+            respuesta=0      
+        print(respuesta)  
+        
+        return respuesta
+    def limpiarHistorial(self):
+        self.historialPeso1=[self.peso1]
+        self.historialPeso2=[self.peso2]
+        self.historialPeso3=[self.peso3]
+    
+    def entrenador(self, entradaPesoR,entradaPesoG,entradaPesoB,respuestaDeseada):
+        print(f"PRIMER PESO R {self.peso1} PRIMER PESO G {self.peso2} PRIMER PESO B  {self.peso3}   PRIMER BIAS  {self.bias}")
+        errores =0
+        numeroDeEpocas=100
+        for epoca in range(numeroDeEpocas):
+            
+            for i in range(len(respuestaDeseada)):
+                
+                respuestaActual = self.propagacion(entradaPesoR[i],entradaPesoG[i],entradaPesoB[i])
+                
+                error = respuestaDeseada[i]- respuestaActual
+                #Actualizacion de bias                    
+                self.bias -= self.tasaAprendizaje*error
+                #Actualizacion de pesos r g b
+                self.peso1 = self.peso1 + (self.tasaAprendizaje * error * entradaPesoR[i])#R
+                self.peso2 = self.peso2 + (self.tasaAprendizaje * error * entradaPesoG[i])#G
+                self.peso3 = self.peso3 + (self.tasaAprendizaje * error * entradaPesoB[i])#B      
+                   
+                self.historialPeso1.append(self.peso1)
+                self.historialPeso2.append(self.peso2)
+                self.historialPeso3.append(self.peso3)  
+                print(f" Actualizando peso  R {self.peso1}     actualizando peso G  {self.peso2}   actualizando peso B {self.peso3}   actualizando bias   {self.bias}")
+                if not np.array_equal(respuestaActual,respuestaDeseada[i]):
+                    print(f"Respuesta del pc   {respuestaActual} es diferente de  respuestaActual {respuestaDeseada[i]}")
+                    errores+=1
+        print("     FALLOS    " ,errores)    
+        print(f" PESO  FINAL  R {self.peso1}     PESO FINAL G  {self.peso2}   PESO FINAL B {self.peso3}   FINAL BIAS   {self.bias}")
+                
+
+    def mostrarGraficoPesos(self):
+        plt.plot(self.historialPeso1, label="Peso 1")
+        plt.plot(self.historialPeso2, label="Peso 2")
+        plt.plot(self.historialPeso3, label="Peso 3")
+        plt.xlabel("Epoca")
+        plt.ylabel("Valor del peso")
+        plt.title("Evolución de los pesos durante el Entrenamiento")
+        plt.legend()
+        plt.show()
+                
+    
+  
